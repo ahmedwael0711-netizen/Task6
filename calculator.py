8ec920f2612b05d3c09610ef0cdc4264cd736a43
ahmedwael0711-netizen <ahmedwael0711@gmail.com>
2025-11-04 12:14:37 +0200

    Fixing Error

--- calculator.py
+++ calculator.py
@@ -2,10 +2,10 @@
 
 class Calculator:
     def add(self, a, b):
-        return a + b-1
+        return a + b
 
     def subtract(self, a, b):
-        return a - b+1
+        return a - b
 
     def multiply(self, a, b):
         return a * b 
