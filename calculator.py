aa760a089df58665eb4632bd73fdb9d7d6788468
ahmedwael0711-netizen <ahmedwael0711@gmail.com>
2025-11-04 12:14:05 +0200

    Eroor

--- calculator.py
+++ calculator.py
@@ -2,10 +2,10 @@
 
 class Calculator:
     def add(self, a, b):
-        return a + b
+        return a + b-1
 
     def subtract(self, a, b):
-        return a - b
+        return a - b+1
 
     def multiply(self, a, b):
         return a * b 
